cfbbd82da22b737a568bef4150ed917e243bc640
Jin Aoyagi <contact.jin02@gmail.com>
2022-04-16 23:36:37 +0900

    ログイン機能を今回の制作物に落とし込みました

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,42 +1,38 @@
 <?php
 session_start();
 require('../dbconnect.php');
-if (isset($_SESSION['user_id']) && $_SESSION['time'] + 60 * 60 * 24 > time()) {
-    $_SESSION['time'] = time();
+require(dirname(__FILE__) . "/parts/login-check.php");
 
-    if (!empty($_POST)) {
-        $stmt = $db->prepare('INSERT INTO events SET title=?');
-        $stmt->execute(array(
-            $_POST['title']
-        ));
-
-        header('Location: http://' . $_SERVER['HTTP_HOST'] . '/admin/index.php');
-        exit();
-    }
+// 権限判定
+if ($_SESSION['right_id'] === 2) {
+  $title = '管理者ページ';
+} elseif ($_SESSION['right_id'] === 1) {
+  $title = '共同管理者ページ';
+} elseif ($_SESSION['right_id'] === 0) {
+  $title = 'エージェント様向けページ';
 } else {
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/admin/login.php');
-    exit();
+  $title = 'error';
+  exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>管理者ログイン</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $title; ?></title>
 </head>
 
 <body>
-    <div>
-        <h1>管理者ページ</h1>
-        <form action="/admin/index.php" method="POST">
-            イベント名：<input type="text" name="title" required>
-            <input type="submit" value="登録する">
-        </form>
-        <a href="/index.php">イベント一覧</a>
-    </div>
+  <div>
+    <h1><?= $title; ?></h1>
+    <form action="" method="POST">
+      <input type="submit" name="logout" value="ログアウト">
+    </form>
+  </div>
 </body>
 
 </html>
