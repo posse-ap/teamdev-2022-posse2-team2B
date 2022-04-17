4897edeb94b1a734a3b46989749eba62b482ca0e
Rei Tanoue <tanouser463@gmail.com>
2022-04-17 11:46:07 +0900

    文字化けの修正

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -1,3 +1,12 @@
+set
+  character_set_client = utf8mb4;
+
+set
+  character_set_connection = utf8mb4;
+
+set
+  character_set_results = utf8mb4;
+
 DROP SCHEMA IF EXISTS shukatsu;
 
 CREATE SCHEMA shukatsu;
