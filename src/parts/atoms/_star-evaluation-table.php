d56a29093a3df1c90de5be98b5f62850b47ab93f
ykent <ppscam225@gmail.com>
2022-04-24 11:46:48 +0900

    少しだけ進めた

---
+++ src/parts/atoms/_star-evaluation-table.php
@@ -0,0 +1,14 @@
+<table>
+        <tr>
+          <td><p>評価項目</p></td>
+          <td class="star-evaluation"><p>★★★☆☆</p></td>
+        </tr>
+        <tr>
+          <td><p>評価項目</p></td>
+          <td class="star-evaluation"><p>★★★☆☆</p></td>
+        </tr>
+        <tr>
+          <td><p>評価項目</p></td>
+          <td class="star-evaluation"><p>★★★☆☆</p></td>
+        </tr>
+      </table>
---
+++ src/parts/molecules/_agent-card-btns.php
@@ -0,0 +1,5 @@
+<?php
+    include(dirname(__FILE__) . '/../atoms/_put-into-inquiry-box-btn.php');
+    include(dirname(__FILE__) . '/../atoms/_put-out-of-inquiey-box.php');
+    include(dirname(__FILE__) . '/../atoms/_view-more-info-btn.php');
+    ?>
