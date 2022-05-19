61bee91e6fedb4d194429f059429bb43d55044ec
Rei Tanoue <tanouser463@gmail.com>
2022-05-19 18:34:12 +0900

    ログイン失敗時に警告文をアラートで出すようにした

--- src/admin/login.php
+++ src/admin/login.php
@@ -27,9 +27,16 @@ if (!empty($_POST)) {
     exit();
   } else {
     echo 'メールアドレスまたはパスワードが間違っています。';
+    $a = true;
   }
 }
 ?>
+
+<script type="text/javascript">
+  <?php if (isset($a)) { ?>
+    alert("メールアドレスまたはパスワードが間違っています。");
+  <?php } ?>
+</script>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -54,14 +61,14 @@ if (!empty($_POST)) {
   <main>
     <div class="wrapper">
       <div class="container">
-        <h1>管理者ログイン</h1>
-        <form action="/admin/login.php" method="POST" class=form-wrapper>
+        <h1 class="login__title">管理者ログイン</h1>
+        <form action=" /admin/login.php" method="POST" class=form-wrapper>
           <label>メールアドレス<input type="email" name="email" required></label>
           <br>
           <label>パスワード<input type="password" required name="password"></label>
           <br>
           <div class="submit">
-            <input type="submit" value="ログイン">
+            <input type="submit" value="ログイン" class="btn">
           </div>
         </form>
       </div>
