219bf3a9706d26ad479574748089e683a0ac9b71
Jin Aoyagi <contact.jin02@gmail.com>
2022-05-19 18:24:47 +0900

    Update reg-info.php
    不要なコメント削除など

--- src/admin/reg-info.php
+++ src/admin/reg-info.php
@@ -5,41 +5,6 @@ require(dirname(__FILE__) . "/app/dbconnect.php"); //データベース接続
 require(dirname(__FILE__) . "/app/login-check.php"); //ログイン判定 未ログインの場合ログインページに遷移
 require(dirname(__FILE__) . "/app/_ctrl-pages.php"); //管理画面の全ページの情報を保持
 
- // テーブルに入れるデータ
-// $trs_stmt = $db->prepare(
- // "SELECT
- //       agents.agent_name,
- //       agent_contract.address,
- //       agents.url,
- //       agent_contract.tel,
- //       agent_contract.pres_name,
- //       agent_contract.pic_name,
- //       agent_contract.pic_tel,
- //       agent_contract.pic_email,
- //       agent_contract.notification_email,
- //       agents.start_at,
- //       agents.expires_at
- //     FROM
- //       agent_contract
- //     LEFT JOIN
- //       agents
- //     ON
- //       agent_contract.agent_id=:agents.id
-
-// //       "
-  // "SELECT
-  //   *
-  // FROM
-  //   agents
-  // LEFT JOIN agent_contract ON agents.id = agent_contract.agent_id
-  // WHERE
-  //   agent_id = :agent_id"
-// );
-// $trs_stmt->execute([':agent_id' => $_SESSION['agent_id']]);
-// $trs = $trs_stmt->fetch();
-//$trs = $trs_stmt->fetch(PDO::FETCH_ASSOC);
-//$pgdata['table_data']['tr'] = $trs;
-
 $pgdata = array();
 $pgdata += array('right_id' => $_SESSION['right_id']);
 $pgdata += array('page_id' => 7);
@@ -47,24 +12,6 @@ $pgdata += array('page_title' => $pages[$pgdata['page_id']]['title']);
 $pgdata += array('table_data' => [
   'th' => ['会社名', '会社所在地', '会社URL', '会社電話番号', '代表者氏名', '担当者氏名', '担当者電話番号', '担当者メールアドレス', '通知用メールアドレス', '契約開始', '契約終了']
 ]);
-
-
-// if ($trs) {
-//   $pgdata += array('info_table' => [
-//     'tr' => [
-//       '会社名' => $trs['agent_name'],
-//       '会社所在地' => $trs['address'],
-//       '会社URL' => $trs['url'],
-//       '会社電話番号' => $trs['tel'],
-//       '代表者氏名' => $trs['pres_name'],
-//       '担当者氏名' => $trs['pic_name'],
-//       '担当者電話番号' => $trs['pic_tel'],
-//       '担当者メールアドレス' => $trs['pic_email'],
-//       '通知用メールアドレス' => $trs['notification_email'],
-//       '契約期間' => $trs['start_at'] . '~' . $trs['expires_at']
-//     ]
-//   ]);
-// }
 
 $agent_id = $_GET['id'];
 // テーブルに追加するデータ
@@ -86,9 +33,7 @@ $trs_stmt = $db->query(
     LEFT JOIN
       agents
     ON
-      agent_contract.agent_id=agents.id
-
-      "
+      agent_contract.agent_id=agents.id"
 );
 $trs = $trs_stmt->fetch(PDO::FETCH_ASSOC);
 $pgdata['table_data']['tr'] = $trs;
