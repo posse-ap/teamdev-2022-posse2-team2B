bc8fe6d42dad175d474b72fd1f0118258de64f13
Rei Tanoue <tanouser463@gmail.com>
2022-05-25 23:00:58 +0900

    アカウント作成・変更ページに飛んだときのデータの引き継ぎ

--- src/admin/parts/molecules/_agent-detail-table.php
+++ src/admin/parts/molecules/_agent-detail-table.php
@@ -7,7 +7,7 @@
       </tr>
     <?php endforeach; ?>
   </table>
-  <p>エージェント情報の書き換えを直接行うことが可能です。<br>変更後、下の「変更」を押してください。</p>
+  <p>エージェント情報の書き換えを直接行うことが可能です。<br>変更後、下の「上記の内容に変更」ボタンを押してください。</p>
 
-  <a href=""><input value=" 変更" type="submit" class="btn detail-btn"></input></a>
+  <a href=""><input value="上記の内容に変更" type="submit" class="btn detail-btn"></input></a>
 </form>
