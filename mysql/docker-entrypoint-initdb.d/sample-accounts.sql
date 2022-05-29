83723c2e75d99d4e2b6b40143bff723340e4a950
Jin Aoyagi <contact.jin02@gmail.com>
2022-05-29 23:19:59 +0900

    アカウント名変更

--- mysql/docker-entrypoint-initdb.d/sample-accounts.sql
+++ mysql/docker-entrypoint-initdb.d/sample-accounts.sql
@@ -24,70 +24,70 @@ VALUES
   (
     'agent1@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent1ウサギ',
+    '株式会社DYM ちいかわ',
     1,
     1
   ),
   (
     'agent2@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent2ハチワレ',
+    'レバレジーズ株式会社 ちいかわ',
     2,
     1
   ),
   (
     'agent3@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent3シーサー',
+    '株式会社irodas ちいかわ',
     3,
     1
   ),
     (
     'agent4@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent4シーサー',
+    '株式会社アドプランナー ちいかわ',
     4,
     1
   ),
     (
     'agent5@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent5シーサー',
+    'キャリアスタート株式会社 ちいかわ',
     5,
     1
   ),
     (
     'agent6@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent3シーサー',
+    'NaS株式会社 ちいかわ',
     6,
     1
   ),
     (
     'agent7@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent3シーサー',
+    'シンクトワイス株式会社 ちいかわ',
     7,
     1
   ),
     (
     'agent8@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent3シーサー',
+    'HRクラウド株式会社 ちいかわ',
     8,
     1
   ),
     (
     'agent9@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent3シーサー',
+    'ポート株式会社 ちいかわ',
     9,
     1
   ),
     (
     'agent10@test.com',
     sha1('teamdev'),
-    '株式会社sampleagent3シーサー',
+    '株式会社マイナビ ちいかわ',
     10,
     1
   );
